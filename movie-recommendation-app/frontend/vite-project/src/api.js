655f8cd213cdd40669187bedb522967c852cb7fa
ankit chaudhary <ac329888@gmail.com>
2026-01-10 19:31:37 +0530

    this is third commit

--- movie-recommendation-app/frontend/vite-project/src/api.js
+++ movie-recommendation-app/frontend/vite-project/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const getRecommendations = async (preference) => {
-  const res = await axios.post("https://movie-recommandation-app-2.onrender.com/", {
-    preference,
-  });
+  const res = await axios.post(
+    `${API_URL}/recommend`,
+    { preference }
+  );
   return res.data.movies;
 };
