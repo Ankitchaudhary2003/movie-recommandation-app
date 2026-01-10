8835d871329dc080ff05b626fc5bde12c193cd89
ankit chaudhary <ac329888@gmail.com>
2026-01-10 19:42:04 +0530

    fourth coommmit

--- movie-recommendation-app/frontend/vite-project/src/api.js
+++ movie-recommendation-app/frontend/vite-project/src/api.js
@@ -3,9 +3,12 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getRecommendations = async (preference) => {
+  console.log("API_URL =>", API_URL); // DEBUG
+
   const res = await axios.post(
     `${API_URL}/recommend`,
     { preference }
   );
+
   return res.data.movies;
 };
